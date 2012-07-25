5f6788e6afab2272dee8e8f3c5b642a8ecaec0f6
nicolask <nicolas.krueger@gmail.com>
2012-07-25 15:20:38 +0200

    moved the config.php to classes/default_config.php and added a new config.dist.php as an example for configuration

---
+++ config.dist.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * copy this file and name it config.php
+ * then modify to match your needs
+ * 
+ * you can override all the options available in classes/default_config.php
+ * 
+ * you must at least set your database connection and url here
+ * 
+ */
+
+$CONF['dbhost']='localhost';
+$CONF['dbname']='mysqldatabase';
+$CONF['dbuser']='mysqluser';
+$CONF['dbpass']='databasepassword';
+
+// This should be the URL to your pastebin. eg: http://paste.info.tm/ or http://paste.info.tm/subdir/
+$CONF['url']='http://paste.info.tm/';// Make sure you end it with a forward slash! (/)
+
+// What is the name of the template you want to use (the folder as displayed in /templates/)
+$CONF['template']='default';
+
+// Site title (Appears in the <title></title> tags)
+$CONF['title']='PASTE - The name says it all.';

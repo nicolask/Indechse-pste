92af037716bcb755bd3c376ea2a60c00b7e73ab4
Nicolas Krueger <nicolas.krueger@gmail.com>
2012-07-26 10:17:15 +0200

    some refactoring, usage of Config and Request. wrapped the whole logic in a try-catch-block so PDOException will be displayed in browser now

--- index.php
+++ index.php
@@ -16,88 +16,104 @@
 
 // Includes
 require_once('classes/config.inc.php');
+
 date_default_timezone_set($CONF['timezone']);
+error_reporting(-1);
+
 require_once('classes/geshi/geshi.php');
 require_once('classes/diff.php');
 require_once('classes/paste.php');
 
+require_once('classes/Request.php');
+$request = Request::getInstance();
+$config = Registry::getInstance()->config;
+
+
+try {
 // Create our pastebin object
-$pastebin = new Pastebin($CONF);
+    $pastebin = new Pastebin($CONF);
 
 /// Clean up older posts 
-$pastebin->doGarbageCollection();
+    $pastebin->doGarbageCollection();
 
 // Process new posting
-$errors = array();
-if (isset($_POST['paste'])) { /* Process posting and redirect */
-    $id = $pastebin->doPost($_POST);
-    if ($id) {
-        $pastebin->redirectToPost($id);
-        exit;
+    $errors = array();
+    if ($request->hasParam('paste')) { /* Process posting and redirect */
+        $id = $pastebin->doPost($_POST);
+        if ($id) {
+            $pastebin->redirectToPost($id);
+            exit;
+        }
     }
-}
 
 // Process downloads.
-if (isset($_GET['dl'])) {
-    global $errors;
-    if (isset($_GET['pass'])) {
-        $getPass = $_GET['pass'];
-    }
-    $pid = intval($_GET['dl']);
-    // If password protected, don't allow to download it.
-    mysql_connect($CONF['dbhost'], $CONF['dbuser'], $CONF['dbpass']) or die(mysql_error());
-    $newPID = mysql_real_escape_string($pid);
-    mysql_select_db($CONF['dbname']) or die(mysql_error());
-    $result = mysql_query("SELECT * from paste where pid = " . $newPID);
+    if (isset($_GET['dl'])) {
+        global $errors;
+        if (isset($_GET['pass'])) {
+            $getPass = $_GET['pass'];
+        }
+        $pid = intval($_GET['dl']);
+        // If password protected, don't allow to download it.
+        mysql_connect($CONF['dbhost'], $CONF['dbuser'], $CONF['dbpass']) or die(mysql_error());
+        $newPID = mysql_real_escape_string($pid);
+        mysql_select_db($CONF['dbname']) or die(mysql_error());
+        $result = mysql_query("SELECT * from paste where pid = " . $newPID);
 
-    if ($result == FALSE) {
-        echo "Paste $pid is not available.";
-        exit;
-    }
+        if ($result == FALSE) {
+            echo "Paste $pid is not available.";
+            exit;
+        }
 
-    $row = mysql_fetch_array($result);
-    $pass = $row['password'];
+        $row = mysql_fetch_array($result);
+        $pass = $row['password'];
 
-    if ($pass == 'EMPTY') {
-        $pastebin->doDownload($pid);
-        exit;
-    } else if ($pass != $getPass) {
-        
-    } else {
-        $pastebin->doDownload($pid);
-        exit;
+        if ($pass == 'EMPTY') {
+            $pastebin->doDownload($pid);
+            exit;
+        } else if ($pass != $getPass) {
+            
+        } else {
+            $pastebin->doDownload($pid);
+            exit;
+        }
     }
-}
 
 // If we get this far, we're going to be displaying some HTML, so let's kick off here.
-$page = array();
+    $page = array();
 
 // Figure out some nice defaults.
-$page['current_format'] = $CONF['default_highlighter'];
-$page['expiry'] = $CONF['default_expiry'];
-$page['remember'] = '';
+    $page['current_format'] = $config->default_highlighter;
+    $page['expiry'] = $config->default_expiry;
+    $page['remember'] = '';
 
 // Add list of recent posts.
-$list = isset($_REQUEST["list"]) ? intval($_REQUEST["list"]) : 10;
-$page['recent'] = $pastebin->getRecentPosts($list);
+    $list = intval($request->getParam('list', 10));
+    $page['recent'] = $pastebin->getRecentPosts($list);
 
 // Show a post.
-if (isset($_REQUEST["show"])) {
-    $pid = intval($_REQUEST['show']);
-    // Get the post.
-    $page['post'] = $pastebin->getPaste($pid);
-    // Ensure corrent format is selected.
-    $page['current_format'] = $page['post']['format'];
-} else {
-    $page['posttitle'] = 'New posting';
-}
+    if ($request->hasParam('show')) {
+        $pid = intval($request->getParam('show'));
+        // Get the post.
+        $page['post'] = $pastebin->getPaste($pid);
+        // Ensure corrent format is selected.
+        $page['current_format'] = $page['post']['format'];
+    } else {
+        $page['posttitle'] = 'New posting';
+    }
 
-if ($page['current_format'] != 'text') {
-    // Give the page a title which features the syntax used.
-    $page['title'] = $CONF['geshiformats'][$page['current_format']] . " - " . $CONF['title'];
-} else {
-    $page['title'] = $CONF['title'];
-}
+    if ($page['current_format'] != 'text') {
+        $geshiformats = $config->get('geshiformats');
+        // Give the page a title which features the syntax used.
+        $page['title'] = $geshiformats[$page['current_format']] . " - " . $config->title;
+    } else {
+        $page['title'] = $config->title;
+    }
 
 // HTML page output.
-include('templates/' . $CONF['template'] . '/theme.php');
+    include('templates/' . $config->template . '/theme.php');
+} catch (Exception $ex) {
+    header('Content-type: text/plain');
+    echo "An error occured\n";
+    echo $ex->getMessage() . "\n";
+    echo $ex->getTraceAsString();
+}

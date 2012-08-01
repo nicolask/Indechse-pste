a85fde439e4dd63a77c5185c7c37ddce979082e9
Nicolas Krueger <nicolas.krueger@gmail.com>
2012-08-01 22:20:02 +0200

    renamed schema file for postgres

--- database/schema/mysql.sql
+++ database/schema/mysql.sql
@@ -12,3 +12,12 @@ CREATE TABLE IF NOT EXISTS `paste` (
   `password` varchar(250) default NULL,
   PRIMARY KEY  (`pid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+CREATE TABLE `dbrev` (
+    `id` int(11) NOT NULL auto_increment,
+    `revision` varchar(32) NOT NULL,
+    `updated` datetime NOT NULL DEFAULT NOW(),
+    `updatename` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE (`revision`, `updatename`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;

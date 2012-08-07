54fa6edc0d68dedb1d96313e5b1bcaff16b425e0
Nicolas Krueger <nicolas.krueger@gmail.com>
2012-08-07 09:45:58 +0200

    renamed pgsql to Pgsql - duuh :(

---
+++ classes/Pste/Db/Pgsql/User.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Description of User
+ *
+ * @author Nicolas Krueger <krueger@white-paper-media.de>
+ */
+include_once('Pste/Db/User.php');
+class Pste_Db_Pgsql_User extends Pste_Db_User
+{
+    
+}

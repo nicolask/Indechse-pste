c3868d0ff5a6b62d6969ff2f5fbf08b7077fd123
Nicolas Krueger <nicolas.krueger@gmail.com>
2012-08-07 07:04:11 +0200

    added view helper to add custom css from views

---
+++ classes/Pste/View/helpers/HeadStyle.php
@@ -0,0 +1,31 @@
+<?php
+class Pste_View_Helper_HeadStyle 
+{
+    private static $_style = array();
+    
+    public function addStyle($style=null, $name=null) {
+        if (isset(self::$_style[$name])) {
+            self::$_style[$name] .= $style;
+        } else {
+            self::$_style[$name] = $style;
+        }
+    }
+    
+    public function headStyle($args) {
+        $numArgs = count($args);
+        if ($numArgs == 1) {
+            $this->addStyle($args[0], 'default');
+        } else if ($numArgs == 2) {
+            $this->addStyle($args[0], $args[1]);
+        }
+    }
+    
+    public function __toString()
+    {
+        $styles = '';
+        foreach (self::$_style as $name => $item) {
+            $styles .= '<style  type="text/css" id="'.$name.'">'.$item.'</style>'."\n"; 
+        }
+        return $styles;
+    }
+}

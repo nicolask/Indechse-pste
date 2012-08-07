35f20210daa6256abbed9bab75cc19de4a762e06
Nicolas Krueger <nicolas.krueger@gmail.com>
2012-08-08 01:04:13 +0200

    added paging and a few icons for list navigation to archive

--- templates/default/components/paste_archive.php
+++ templates/default/components/paste_archive.php
@@ -1,15 +1,59 @@
 <div class="paste">
     <h1>Archive (<?= $this->count ?> pastes available)</h1>
-    <div>
+    <div class="paginator">
         <?php
-        $pageNum = 1;
+            $maxPage = ((int)($this->count/$this->itemsPerPage))+1
+        ?>
+        <?php if ($this->page > 2) : ?>
+        <a href="<?=$this->url ?>?archive&page=1">
+            <img src="templates/default/images/gtk_goto_first_ltr.png" />
+        </a> 
+        <?php else: ?>
+        <img src="templates/default/images/gtk_goto_first_ltr.png" />
+        <?php endif; ?>
+        <?php if ($this->page > 1) : ?>
+        <a href="<?=$this->url ?>?archive&page=<?= $this->page-1 ?>">
+            <img src="templates/default/images/gtk_go_forward_rtl.png" />
+        </a>
+        <?php else: ?>
+        <img src="templates/default/images/gtk_go_forward_rtl.png" />
+        <?php endif; ?>
+        <span class="page-enum">
+        <?php
+        $pageNum = $this->page-2;
+        $maxNum = $this->page+2;
+        $minDiff = $pageNum - 1;
+        $maxDiff = $maxPage - $maxNum;
+        if ($maxDiff < 0 && $minDiff > 0) {
+            $pageNum -= abs($maxDiff) > $minDiff ? $minDiff : abs($maxDiff);
+        }
+        if ($minDiff < 0 && $maxDiff > 0) {
+            $maxNum += abs($minDiff) > $maxDiff ? $maxDiff : abs($minDiff);
+        }
+        $pageNum = $pageNum < 1 ? 1 : $pageNum;
+        $maxNum = $maxNum > $maxPage ? $maxPage : $maxNum;
+        
         do {
             $style = $pageNum == $this->page ? 'font-size: 1.5em' : '';
             printf('<a style="padding-right:10px; %3$s" href="%1$s?archive&page=%2$s">%2$s</a>', $this->url, $pageNum, $style);
 
             $pageNum++;
-        } while ((($pageNum - 1) * $this->itemsPerPage) <= $this->count);
+        } while ($pageNum <= $maxNum);
         ?>
+        </span>
+        <?php if ($this->page+1 <= $maxPage) :  ?>
+        <a href="<?=$this->url ?>?archive&page=<?= $this->page+1 ?>"><img src="templates/default/images/gtk_go_forward_ltr.png" /></a>
+        <?php else: ?>
+        <img src="templates/default/images/gtk_go_forward_ltr.png" />
+        <?php endif; ?>
+        <?php if ($this->page+1 < $maxPage) :  ?>
+        <a href="<?=$this->url ?>?archive&page=<?= $maxPage ?>"><img src="templates/default/images/gtk_goto_last_ltr.png" /></a> 
+        <?php else: ?>
+        <img src="templates/default/images/gtk_goto_last_ltr.png" />
+        <?php endif; ?>
+        
+        <br />
+        
     </div>
 
     <table class="archive">

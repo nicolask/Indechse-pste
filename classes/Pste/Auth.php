086c6dd6d062b3949670cdce63bba0c48e07484b
Nicolas Krueger <nicolas.krueger@gmail.com>
2012-08-07 05:31:25 +0200

    added logic and db-accessor for user auth

---
+++ classes/Pste/Auth.php
@@ -0,0 +1,80 @@
+<?php
+class Pste_Auth
+{
+    /**
+     *
+     * @var Pste-Request_Abstract $_request 
+     */
+    private $_request;
+    
+    private $_user;
+    
+    private $_authenticated;
+    
+    /**
+     *
+     * @param Pste_Request_Abstract $request 
+     */
+    public function __construct(Pste_Request_Abstract $request) {
+        $this->_request = $request;
+    }
+    
+    public function isAuthenticated() {
+        if ($this->_request->hasParam('logout')) {
+            $this->_logout();
+            return false;
+        }
+        
+        if ($this->_request->hasParam('auth')) {
+            $this->_authenticated = $this->_checkLogin();
+        } else {
+            $this->_authenticated = $this->_checkSession();
+        }
+        if ($this->_authenticated) {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    protected function _checkLogin() {
+        if (!($this->_request->hasParam('username') && $this->_request->hasParam('password'))) {
+            return false;
+        }
+        
+        $username = $this->_request->getParam('username', '');
+        $password = $this->_request->getParam('password', '');
+        
+        $user = new Pste_User();
+        $this->_user = $user;
+        $ok = $user->find($username, $password);
+        if ($ok) {
+            session_start();
+            $_SESSION['user'] = $user->toArray();
+            return true;
+        }
+        return false;
+    }
+    
+    protected function _checkSession() {
+        session_start();
+        if (!isset($_SESSION['user'])) {
+            return false;
+        }
+        
+        $user = new Pste_User();
+        $user->setFromArray($_SESSION['user']);
+        $this->_user = $user;
+        return true;
+    }
+    
+    protected function _logout() {
+        session_start();
+        session_unset();
+        session_destroy();
+    }
+    
+    public function getUser() {
+        return $this->_user;
+    }
+}
---
+++ classes/Pste/User.php
@@ -0,0 +1,67 @@
+<?php
+class Pste_User
+{
+    protected $_conn;
+    protected $_id;
+    protected $_username;
+    protected $_profile = null;
+    
+    public function __construct($id=null) {
+        $this->_conn = Pste_Database::getInstance()->getConnection();
+        if ($id) {
+            $this->_id = $id;
+            $this->_populate();
+        }
+    }
+    
+    public function find($username, $password) {
+        $encPw = sha1($password);
+        $sql = 'SELECT id, username FROM "user" WHERE username = :username AND password = :password';
+        $stmt = $this->_conn->prepare($sql);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', $encPw);
+        $result = $stmt->execute();
+        
+        if ($result) {
+            $data = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            if (!$data) return false;
+            
+            $this->_populate($data);
+            return true;
+        }
+        
+        return false;
+        
+    }
+    
+    protected function _populate($data=null) {
+        if (is_array($data)) {
+            $this->_id = $data['id'];
+            $this->_username = $data['username'];
+        }
+        
+        /**
+         *  @todo implement user profile
+         */
+    }
+    
+    public function toArray() {
+        return array('id' => $this->_id, 'username' => $this->_username);
+    }
+    
+    public function setFromArray($data) {
+        $this->_id = $data['id'];
+        $this->_username = $data['username'];
+    }
+    
+    public function getUsername() {
+        return $this->_username;
+    }
+    
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+}
